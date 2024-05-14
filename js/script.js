5aafe9b94f2cea74828b4ea48418f02ad7e9a89c
furkannnyzg <maxuxzone@gmail.com>
2024-05-14 23:34:30 +0300

    Ana panel tasarım
    Ana panel tasarım logosu düzenlendi ve hazırlandı.Çekmece çalışır hale getirildi.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 
 const navigation = document.querySelector('.drawer-content');
+const navigation2=document.querySelector('.drawer-header');
 document.querySelector('.navbar-toggler').onclick = function(){
     this.classList.toggle('active');
     navigation.classList.toggle('active'); 
+    navigation2.classList.toggle('active');
 }
 
