2c65b7cf5a71a26d49a5e926f7e6ceb8bc03d849
furkannnyzg <maxuxzone@gmail.com>
2024-05-15 18:57:41 +0300

    Sekmelerin ve Ana Dashboard
    Dashboard tasarımı ve html bağlama işlemleri gerçekleşti.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 
 const navigation = document.querySelector('.drawer-content');
 const navigation2=document.querySelector('.drawer-header');
+const content_area=document.querySelector('.content-area');
+const two_screen=document.querySelector('.two-screen');
 document.querySelector('.navbar-toggler').onclick = function(){
     this.classList.toggle('active');
     navigation.classList.toggle('active'); 
     navigation2.classList.toggle('active');
+    content_area.classList.toggle('active');
+    two_screen.classList.toggle('active');
 }
+
+
 
