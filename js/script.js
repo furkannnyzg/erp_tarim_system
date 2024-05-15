0e7a848fbdac4fe8cda923f2161d2345341635c7
furkannnyzg <maxuxzone@gmail.com>
2024-05-16 00:39:16 +0300

    Ana Panel Frame
    Ana panele frame bağlandı.

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 const navigation = document.querySelector('.drawer-content');
 const navigation2=document.querySelector('.drawer-header');
 const content_area=document.querySelector('.content-area');
-const two_screen=document.querySelector('.two-screen');
+const i_frame=document.querySelector('.i-frame');
 document.querySelector('.navbar-toggler').onclick = function(){
     this.classList.toggle('active');
     navigation.classList.toggle('active'); 
     navigation2.classList.toggle('active');
     content_area.classList.toggle('active');
-    two_screen.classList.toggle('active');
+    i_frame.classList.toggle('active');
 }
 
 
