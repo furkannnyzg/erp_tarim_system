9631feb297b1a4d4bc7839c8f488815df3108fea
furkannnyzg <maxuxzone@gmail.com>
2024-05-16 19:12:03 +0300

    İkonlar ve Sekmeler
    İkonlar ve sekmeler düzenlendi.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-
 const navigation = document.querySelector('.drawer-content');
-const navigation2=document.querySelector('.drawer-header');
-const content_area=document.querySelector('.content-area');
-const i_frame=document.querySelector('.i-frame');
-document.querySelector('.navbar-toggler').onclick = function(){
+const navigation2 = document.querySelector('.drawer-header');
+const iframeContainer = document.querySelector('.iframe-container');
+document.querySelector('.navbar-toggler').onclick = function() {
     this.classList.toggle('active');
-    navigation.classList.toggle('active'); 
+    navigation.classList.toggle('active');
     navigation2.classList.toggle('active');
-    content_area.classList.toggle('active');
-    i_frame.classList.toggle('active');
-}
-
-
-
+    iframeContainer.classList.toggle('active');
+};
